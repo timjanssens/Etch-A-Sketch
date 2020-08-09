b84cbf09acfba7a33ebe0b86617548b903d1f5c5
Tim Janssens <47323747+timjanssens@users.noreply.github.com>
2020-08-09 09:47:07 +0200

    Seperate functions from event listener

--- script.js
+++ script.js
@@ -1,37 +1,37 @@
-
-let totalDivsPerRow = 36;
-let containerDiv = document.querySelector('#containerDiv');
-
-
-for (let i = 0; i < totalDivsPerRow; i++) {
-
-    let divRow = document.createElement('div');
-
+function createDivs(totalDivsPerRow) {
     for (let i = 0; i < totalDivsPerRow; i++) {
-
-        let divEl = document.createElement('div');
-        divEl.style.display = 'inline-block'
-        //calculate width from div
-        let widthDiv = (660) / totalDivsPerRow;
-        let widthDivPx = widthDiv + 'px';
-        divEl.style.width = widthDivPx;
-        divEl.style.height = widthDivPx;
-        divEl.className = 'innerDiv';
-        divEl.style.border = '1px solid black';
         
-
-        divRow.appendChild(divEl);
+        let divRow = document.createElement('div');
+        
+        for (let i = 0; i < totalDivsPerRow; i++) {
+            
+            let divEl = document.createElement('div');
+            divEl.style.display = 'inline-block'
+            //calculate width from div
+            let widthDiv = (600) / totalDivsPerRow;
+            let widthDivPx = widthDiv + 'px';
+            divEl.style.width = widthDivPx;
+            divEl.style.height = widthDivPx;
+            divEl.className = 'innerDiv';
+            divEl.style.border = '1px solid black';
+            
+            
+            divRow.appendChild(divEl);
+        }
+        
+        
+        
+        let containerDiv = document.querySelector('#containerDiv');
+        containerDiv.appendChild(divRow);
     }
+}
 
-   
-
-    containerDiv.appendChild(divRow);
+function blackWhenHoverOver(e){
+    e.preventDefault();
+    e.target.style.backgroundColor = 'black';
 }
 
 
 
-containerDiv.addEventListener('mouseover',function(e){
-  //  if (containerDiv.className == '#innerDiv') {
-        e.target.style.backgroundColor = 'black';
-    //}
-});
+containerDiv.addEventListener('mouseover', blackWhenHoverOver);
+window.addEventListener(onload, createDivs(36));
