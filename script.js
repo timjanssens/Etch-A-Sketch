14bf29cddde00bacd3024acbd77049364e152567
Tim Janssens <47323747+timjanssens@users.noreply.github.com>
2020-08-09 11:56:35 +0200

    Add functions and buttons clear, change squares, colors

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
+let changeColor = false;
+//functions
 function createDivs(totalDivsPerRow) {
     for (let i = 0; i < totalDivsPerRow; i++) {
-        
+
         let divRow = document.createElement('div');
-        
+
         for (let i = 0; i < totalDivsPerRow; i++) {
-            
+
             let divEl = document.createElement('div');
             divEl.style.display = 'inline-block'
             //calculate width from div
@@ -14,24 +16,74 @@ function createDivs(totalDivsPerRow) {
             divEl.style.height = widthDivPx;
             divEl.className = 'innerDiv';
             divEl.style.border = '1px solid black';
-            
-            
+
+
             divRow.appendChild(divEl);
         }
-        
-        
-        
+
+
+
         let containerDiv = document.querySelector('#containerDiv');
         containerDiv.appendChild(divRow);
     }
 }
 
-function blackWhenHoverOver(e){
+function changeWhenHoverOver(e) {
     e.preventDefault();
-    e.target.style.backgroundColor = 'black';
+    if (!changeColor) {
+        e.target.style.backgroundColor = 'black';
+    } else {
+        let randomR = Math.floor(Math.random() * 257);
+        let randomG = Math.floor(Math.random() * 257);
+        let randomB = Math.floor(Math.random() * 257);
+
+        e.target.style.backgroundColor = `rgb(${randomR},${randomG},${randomB})`;
+    }
 }
 
 
+function clearGrid() {
+    let allInnerDivs = document.querySelectorAll('.innerDiv');
+    allInnerDivs.forEach(element => {
+        element.style.backgroundColor = 'white';
+    });
+}
 
-containerDiv.addEventListener('mouseover', blackWhenHoverOver);
-window.addEventListener(onload, createDivs(36));
+function removeAllInnerDivs() {
+    let allInnerDivs = document.querySelectorAll('.innerDiv');
+    allInnerDivs.forEach(element => {
+        element.remove();
+    });
+}
+
+
+function changeSquares() {
+    let newAmountOfSquares = prompt('How many squares in one row do you want (max128)? ');
+    if (newAmountOfSquares > 0 && newAmountOfSquares < 129) {
+        removeAllInnerDivs();
+        createDivs(newAmountOfSquares);
+    } else
+        alert('That was not a number between 1 and 128');
+}
+
+function changeToColors() {
+    changeColor = true;
+}
+
+function changeToBW() {
+    changeColor = false;
+}
+
+function test(e) {
+    e.preventDefault();
+    console.log(12);
+}
+
+//event handlers
+
+containerDiv.addEventListener('mouseover', changeWhenHoverOver);
+window.addEventListener('load', createDivs(16));
+document.querySelector('#clearBtn').addEventListener('click', clearGrid);
+document.querySelector('#changeSquaresBtn').addEventListener('click', changeSquares)
+document.querySelector('#colorFullBtn').addEventListener('click', changeToColors);
+document.querySelector('#bwBtn').addEventListener('click', changeToBW);
